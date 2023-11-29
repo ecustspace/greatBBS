2cc301e7f8393507d7cf4602c05420e0c4db7126
ecustspace <129814711+ecustspace@users.noreply.github.com>
2023-11-29 17:39:13 +0800

    0.1.0

--- app/api/server.js
+++ app/api/server.js
@@ -9,6 +9,7 @@ export const docClient = DynamoDBDocumentClient.from(client);
 export const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
+    secure: false,
     auth: {
         user: process.env.SMTP_USERNAME,
         pass: process.env.SMTP_USERPASS,

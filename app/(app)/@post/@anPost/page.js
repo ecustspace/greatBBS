699233850b234210a4b206f63a7864d209addc31
ecustspace <129814711+ecustspace@users.noreply.github.com>
2023-12-04 20:00:41 +0800

    0.1.0

--- app/(app)/@post/@anPost/page.js
+++ app/(app)/@post/@anPost/page.js
@@ -2,14 +2,6 @@ import CardContainer from "@/app/component/cardContainer";
 import {Url} from "@/app/(app)/clientConfig";
 
 export default async function Home() {
-    const data = await fetch(Url + `/api/getPostData?postType=AnPost`,{next:{tags:['AnPost']}})
-        .then(res => {
-            return res.json()
-        }).catch(() => {
-            return {
-                posts: 'err'
-            }
-        })
     return (
         <div>
             <CardContainer type='AnPost' />
--- app/(app)/@post/@ins/page.js
+++ app/(app)/@post/@ins/page.js
@@ -2,14 +2,6 @@ import {Url} from "@/app/(app)/clientConfig";
 import InsContainer from "@/app/component/insContainer";
 
 export default async function Home() {
-    const data = await fetch(Url + `/api/getPostData?postType=Image`,{next:{tags:['Image']}})
-        .then(res => {
-            return res.json()
-        }).catch(() => {
-            return {
-                posts: 'err'
-            }
-        })
     return (
         <div>
             <InsContainer />

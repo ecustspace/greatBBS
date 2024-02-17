f95f69b806040f22d9442f3428b5860fd0e5bd02
ecustspace <q1563741036@gmail.com>
2024-02-17 16:28:14 +0800

    Changes

--- app/api/postImg/route.js
+++ app/api/postImg/route.js
@@ -63,9 +63,9 @@ export async function POST(request) {
         }).catch(() => {return 'err'})
 
     let image_list = []
-
+    console.log(data.get('file[0]'))
     for (let i = 0; i < fileLength; i++) {
-        const fileData = uploadImage(data.get(`file[${i}]`))
+        const fileData =  uploadImage(data.get(`file[${i}]`))
         image_list.push(fileData)
     }
     image_list = await Promise.all(image_list)

ac4b856f163ad3bb47e9bd95f0bfd9ca4fc0b470
ecustspace <q1563741036@gmail.com>
2024-07-19 22:22:53 +0800

    .

--- app/api/serverAction.js
+++ app/api/serverAction.js
@@ -158,6 +158,7 @@ export async function fetchDataWithPostType(type) {
         }
         posts = [...posts, ...data_.posts]
         keys = data_.keys
+    }
     if (posts.length === 0) {
         return {
             posts: [],
@@ -893,7 +894,7 @@ export async function Report(PK,SK) {
             PK:PK,
             SK:SK
         },
-        ProjectionExpression:'#type',
+        ProjectionExpression:'type',
         ExpressionAttributeNames: {
             '#type': 'Type'
         }
